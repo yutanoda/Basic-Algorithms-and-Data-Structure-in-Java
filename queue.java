e6910ff72f8a059570ad31398804719286263556
yutanoda <atuyuta902@yahoo.co.jp>
2020-02-09 20:57:43 +0900

    queue

---
+++ queue.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Main {
+
+  public static void main (String[] args) {
+	  	Queue queue = new LinkedList();
+		queue.add(2); // [2]
+		queue.add(4); // [2,4]
+		System.out.println(queue.peek()); // prints 2
+		queue.add(5); // [2,4,5]
+		queue.poll();
+		System.out.println(queue); // prints [4,5]
+  }
+}
